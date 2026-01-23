3f26b145f171c750cad2b05808ae436ee9597002
Himalayan Days User <user@himalayandays.com>
2026-01-23 11:00:04 +0530

    Fix: Remove pages config to resolve redirect loop

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,9 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-    pages: {
-        signIn: '/api/auth/signin',
-    },
+    // pages: {
+    //    signIn: '/api/auth/signin',
+    // },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
