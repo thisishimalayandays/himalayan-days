4e54b88116d6b18031bd3bf3c59c979a802671f2
Himalayan Days User <user@himalayandays.com>
2026-01-23 11:40:39 +0530

    Fix: Enable mobile autoplay (playsinline) and 1080p for hero video

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -15,7 +15,7 @@ export function Hero() {
             <div className="absolute inset-0 z-0 select-none pointer-events-none overflow-hidden">
                 <div className="absolute inset-0 bg-black/40 z-10" /> {/* Overlay */}
                 <iframe
-                    src="https://www.youtube.com/embed/S5tGX6o84us?autoplay=1&mute=1&controls=0&loop=1&playlist=S5tGX6o84us&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&modestbranding=1"
+                    src="https://www.youtube.com/embed/S5tGX6o84us?autoplay=1&mute=1&controls=0&loop=1&playlist=S5tGX6o84us&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&modestbranding=1&playsinline=1&vq=hd1080"
                     className="absolute top-1/2 left-1/2 w-[300%] h-[300%] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
